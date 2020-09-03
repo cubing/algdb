c8d94a5f6330ecd7c96e46d9254b693e1b147009
Caleb <choover11@gmail.com>
2020-09-03 14:52:18 -0700

    changed server url to api.algdb.net

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,6 @@
 export const PRODUCTION = process.env.NODE_ENV === 'production'
 export const SERVER_URI =
-  'https://us-central1-algdb-d312e.cloudfunctions.net/api/jql'
+  'https://api.algdb.net/jql'
 export const WCA_LOGIN_REDIRECT: string = PRODUCTION
   ? process.env.REACT_APA_WCA_LOGIN_URL_PROD!
   : process.env.REACT_APP_WCA_LOGIN_URL_DEV!
