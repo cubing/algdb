419beedd571081d1cd093fa41e6b4434e5ba8c55
Caleb <choover11@gmail.com>
2020-09-06 09:21:40 -0700

    can now add algsets

--- client/src/pages/Admin/Algsets.tsx
+++ client/src/pages/Admin/Algsets.tsx
@@ -1,7 +1,80 @@
-import React, { ReactElement } from 'react'
-import { Flex, Heading, Spinner } from '@chakra-ui/core'
+import React, { ReactElement, useState, ChangeEvent, MouseEvent, FormEvent } from 'react'
+import { Flex, Heading, Spinner, Input, Button, Select } from '@chakra-ui/core'
 import useJqlQuery from '../../hooks/useJqlQuery'
-import { AlgsetPaginator } from '../../generated/jql'
+import useJqlMutation from '../../hooks/useJqlMutation'
+import { Maybe, Algset, AlgsetPaginator, Puzzle, PuzzlePaginator } from '../../generated/jql'
+
+type AddAlgsetProps = {
+  onAdd: () => void
+  puzzles: Array<Maybe<Puzzle>>
+}
+
+type AddAlgsetMutationArgs = {
+  name: string
+  puzzle: string
+}
+
+function AddAlgset({ onAdd, puzzles }: AddAlgsetProps): ReactElement {
+  const [algsetName, setAlgsetName] = useState<string>('')
+  const [selectedPuzzle, setSelectedPuzzle] = useState<string>('0')
+
+  const [mutate, { isLoading, error }] = useJqlMutation<
+    Maybe<Algset>,
+    Error,
+    AddAlgsetMutationArgs
+  >('createAlgset', {
+    id: null,
+    name: null,
+  })
+
+  const onClick = async (e: MouseEvent<HTMLButtonElement> | FormEvent) => {
+    e.preventDefault();
+    if (algsetName) {
+      try {
+        await mutate({
+          name: algsetName,
+          puzzle: selectedPuzzle,
+        })
+        onAdd()
+        setAlgsetName('')
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  }
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setAlgsetName(event.target.value)
+  }
+
+  const handleChangePuzzle = (event: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedPuzzle(event.target.value)
+  }
+
+  if (error) {
+    console.error(error);
+  }
+
+  return (
+    <tr>
+      <td />
+      <td>
+        <Input placeholder="OLL" value={algsetName} onChange={handleChange} />
+      </td>
+      <td>
+        <Select value={selectedPuzzle} onChange={handleChangePuzzle}>
+          <option key={0} value="0">Select</option>
+          {puzzles.map((puzzle) => (
+            <option key={puzzle?.id} value={puzzle?.id}>{puzzle?.name}</option>
+          ))}
+        </Select>
+      </td>
+      <td>
+        <Button isLoading={isLoading} isDisabled={!algsetName} onClick={onClick}>Add</Button>
+      </td>
+    </tr>
+  )
+}
 
 const getAlgsetsQuery = {
   paginatorInfo: {
@@ -19,11 +92,23 @@ const getAlgsetsQuery = {
 }
 
 export default function Algsets(): ReactElement {
-  const { isLoading, data, error } = useJqlQuery<AlgsetPaginator, Error>(
+  const { isLoading, data, refetch, error } = useJqlQuery<AlgsetPaginator, Error>(
     'getMultipleAlgset',
     'getMultipleAlgset',
     getAlgsetsQuery,
   )
+
+  const puzzleQuery = useJqlQuery<PuzzlePaginator, Error>(
+    'getMultiplePuzzle',
+    'getMultiplePuzzle',
+    {
+      data: {
+        name: null
+      }
+    }
+  )
+
+  const puzzles = puzzleQuery?.data?.data || [];
 
   const algsets = data?.data || [];
 
@@ -60,6 +145,7 @@ export default function Algsets(): ReactElement {
                 <td>{new Date(algset.created_at * 1000).toLocaleString()}</td>
               </tr>
             ) : false)}
+            <AddAlgset onAdd={refetch} puzzles={puzzles} />
           </tbody>
         </table>
       </Flex>
