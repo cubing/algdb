e4ed335e5a58e33fc2dc842e781c0fb14df0da6f
James <james@thecubicle.us>
2020-09-06 12:03:57 -0400

    backend add algsets lookup to puzzle type

--- backend/functions/src/services/puzzle/puzzle.typeDef.ts
+++ backend/functions/src/services/puzzle/puzzle.typeDef.ts
@@ -1,6 +1,6 @@
-import { generateIdField, generateUpdatedAtField, generateCreatedAtField, generateCreatedByField } from '../../helpers/tier0/typeDef';
+import { generateIdField, generateUpdatedAtField, generateCreatedAtField, generateCreatedByField, generatePaginatorArgs } from '../../helpers/tier0/typeDef';
 
-import { User } from '../services'
+import { User, Algset } from '../services'
 
 import { DataTypes } from "sequelize";
 import { dataTypes } from '../../jql/helpers/dataType';
@@ -28,4 +28,14 @@ export default {
   ...generateCreatedAtField(),
   ...generateUpdatedAtField(),
   ...generateCreatedByField(User),
+  algsets: {
+    type: Algset.__typename,
+    args: generatePaginatorArgs(Algset),
+    resolver: async (context, req, currentObject, query, args, parent) => {
+      return Algset.paginator.getRecord(req, {
+        ...query?.__args,
+        puzzle: currentObject.id
+      }, query);
+    }
+  },
 }
