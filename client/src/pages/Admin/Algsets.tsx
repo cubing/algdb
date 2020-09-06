e196e65d9222109ae76160e3dec3e7b9c7e2359e
Caleb <choover11@gmail.com>
2020-09-06 13:52:58 -0700

    can edit algsets

--- client/src/pages/Admin/Algsets.tsx
+++ client/src/pages/Admin/Algsets.tsx
@@ -1,5 +1,23 @@
 import React, { ReactElement, useState, ChangeEvent, MouseEvent, FormEvent } from 'react'
-import { Flex, Heading, Spinner, Input, Button, Select } from '@chakra-ui/core'
+import {
+  Flex,
+  Heading,
+  Spinner,
+  Input,
+  Button,
+  Select,
+  FormLabel,
+  Stack,
+  Box,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  DrawerFooter,
+  useDisclosure,
+} from '@chakra-ui/core'
 import useJqlQuery from '../../hooks/useJqlQuery'
 import useJqlMutation from '../../hooks/useJqlMutation'
 import { Maybe, Algset, AlgsetPaginator, Puzzle, PuzzlePaginator } from '../../generated/jql'
@@ -9,9 +27,12 @@ type AddAlgsetProps = {
   puzzles: Array<Maybe<Puzzle>>
 }
 
-type AddAlgsetMutationArgs = {
-  name: string
-  puzzle: string
+type AlgsetMutationArgs = {
+  id?: string
+  name?: string
+  puzzle?: string
+  mask?: string
+  visualization?: string
 }
 
 function AddAlgset({ onAdd, puzzles }: AddAlgsetProps): ReactElement {
@@ -21,7 +42,7 @@ function AddAlgset({ onAdd, puzzles }: AddAlgsetProps): ReactElement {
   const [mutate, { isLoading, error }] = useJqlMutation<
     Maybe<Algset>,
     Error,
-    AddAlgsetMutationArgs
+    AlgsetMutationArgs
   >('createAlgset', {
     id: null,
     name: null,
@@ -85,6 +106,11 @@ const getAlgsetsQuery = {
     id: null,
     name: null,
     puzzle: {
+      id: null,
+      name: null,
+    },
+    mask: null,
+    visualization: {
       name: null,
     },
     created_at: null,
@@ -92,6 +118,12 @@ const getAlgsetsQuery = {
 }
 
 export default function Algsets(): ReactElement {
+  const [ editingAlgsetId, setEditingAlgsetId ] = useState<string | undefined>();
+  const [ editingAlgsetName, setEditingAlgsetName ] = useState<string | undefined>();
+  const [ editingAlgsetPuzzle, setEditingAlgsetPuzzle ] = useState<string | undefined>();
+  const [ editingAlgsetVisualization, setEditingAlgsetVisualization ] = useState<string | undefined>();
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const { isLoading, data, refetch, error } = useJqlQuery<AlgsetPaginator, Error>(
     'getMultipleAlgset',
     'getMultipleAlgset',
@@ -108,9 +140,35 @@ export default function Algsets(): ReactElement {
     }
   )
 
-  const puzzles = puzzleQuery?.data?.data || [];
+  const [mutateAlgset, updateAlgsetMutation] = useJqlMutation<
+    Maybe<Algset>,
+    Error,
+    AlgsetMutationArgs
+  >('updateAlgset', {})
 
-  const algsets = data?.data || [];
+  const puzzles = puzzleQuery?.data?.data || []
+
+  const algsets = data?.data || []
+
+  const edit = (algset: Algset) =>
+    (event: MouseEvent) => {
+      onOpen();
+      setEditingAlgsetName(algset.name)
+      setEditingAlgsetPuzzle(algset.puzzle.id)
+      setEditingAlgsetVisualization(algset.visualization.name)
+      setEditingAlgsetId(algset.id)
+    }
+
+  const saveAlgset = async () => {
+    await mutateAlgset({
+      id: editingAlgsetId,
+      name: editingAlgsetName,
+      puzzle: editingAlgsetPuzzle,
+      visualization: editingAlgsetVisualization,
+    })
+    refetch()
+    onClose()
+  }
 
   if (isLoading) {
     return <Spinner />
@@ -133,7 +191,9 @@ export default function Algsets(): ReactElement {
               <th style={{textAlign: 'left'}}>ID</th>
               <th style={{textAlign: 'left'}}>Name</th>
               <th style={{textAlign: 'left'}}>Puzzle</th>
+              <th style={{textAlign: 'left'}}>Visualization</th>
               <th style={{textAlign: 'left'}}>Created</th>
+              <th style={{textAlign: 'left'}}>{' '}</th>
             </tr>
           </thead>
           <tbody>
@@ -142,12 +202,83 @@ export default function Algsets(): ReactElement {
                 <td>{algset.id}</td>
                 <td>{algset.name}</td>
                 <td>{algset.puzzle?.name}</td>
+                <td>{algset?.visualization?.name}</td>
                 <td>{new Date(algset.created_at * 1000).toLocaleString()}</td>
+                <td><Button onClick={edit(algset)}>Edit</Button></td>
               </tr>
             ) : false)}
             <AddAlgset onAdd={refetch} puzzles={puzzles} />
           </tbody>
         </table>
+        <Drawer
+          isOpen={isOpen}
+          onClose={onClose}
+          placement="right"
+          size="md"
+        >
+          <DrawerOverlay>
+            <DrawerContent>
+              <DrawerCloseButton />
+              <DrawerHeader>
+                Editing
+                {' '}
+                {editingAlgsetName}
+              </DrawerHeader>
+              <DrawerBody>
+                <Stack spacing="1.5em">
+                  <Box>
+                    <FormLabel htmlFor="editingAlgsetName">Name</FormLabel>
+                    <Input
+                      value={editingAlgsetName}
+                      onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                        setEditingAlgsetName(event.target.value)
+                      }}
+                      id="editingAlgsetName"
+                      placeholder={editingAlgsetName}
+                    />
+                  </Box>
+
+                  <Box>
+                    <FormLabel htmlFor="editingAlgsetPuzzle">Puzzle</FormLabel>
+                    <Select
+                      value={editingAlgsetPuzzle}
+                      onChange={(event: ChangeEvent<HTMLSelectElement>) => {
+                        setEditingAlgsetPuzzle(event.target.value)
+                      }}
+                      id="editingAlgsetPuzzle"
+                    >
+                      {puzzles.map((puzzle) => (
+                        <option key={puzzle?.id} value={puzzle?.id}>{puzzle?.name}</option>
+                      ))}
+                    </Select>
+                  </Box>
+
+                  <Box>
+                    <FormLabel htmlFor="editingAlgsetVisualization">Visualization</FormLabel>
+                    <Select
+                      value={editingAlgsetVisualization}
+                      onChange={(event: ChangeEvent<HTMLSelectElement>) => {
+                        setEditingAlgsetVisualization(event.target.value)
+                      }}
+                      id="editingAlgsetVisualization"
+                    >
+                      <option value="V_2D">V_2D</option>
+                      <option value="V_3D">V_3D</option>
+                      <option value="V_PG3D">VPg3D</option>
+                    </Select>
+                  </Box>
+
+                </Stack>
+              </DrawerBody>
+              <DrawerFooter>
+                <Button variant="outline" mr={3} onClick={onClose}>
+                  Cancel
+                </Button>
+                <Button color="blue" onClick={saveAlgset} isLoading={updateAlgsetMutation.isLoading}>Save</Button>
+              </DrawerFooter>
+            </DrawerContent>
+          </DrawerOverlay>
+        </Drawer>
       </Flex>
     </Flex>
   )
