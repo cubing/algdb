3debc4a96edb6e958b34b9963423d7149b3a19e6
saranshgrover <contact@saranshgrover.com>
2020-09-06 20:46:16 -0400

    add env variables

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,5 @@
 export const PRODUCTION = process.env.NODE_ENV === 'production'
-export const SERVER_URI =
-  'https://api.algdb.net/jql'
+export const SERVER_URI = 'https://api.algdb.net/jql'
 export const WCA_LOGIN_REDIRECT: string = PRODUCTION
-  ? process.env.REACT_APA_WCA_LOGIN_URL_PROD!
+  ? process.env.REACT_APP_WCA_LOGIN_URL_PROD!
   : process.env.REACT_APP_WCA_LOGIN_URL_DEV!
